9584f951b20633c7020780c33622399ca9d69111
Mike Netz <mike.netz@mike-netz.biz>
2021-12-17 00:25:22 +0100

    Beschreibung basierend auf das Block-Schalt-Bild

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int, char**)
+{
+    // Messen die Geschwindigkeit
+
+    // Filtere die Geschwindigkeit
+
+    // Brechne den Weg
+
+    // Plotte ein Weg-Zeit-Deiagramm
+}
